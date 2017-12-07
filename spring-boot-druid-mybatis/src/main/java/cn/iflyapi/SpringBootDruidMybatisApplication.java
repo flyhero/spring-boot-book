d112402ec55af81ca20ca3bc4d5f53ea4afb1341
flyhero <qfwang666@163.com>
2017-12-07 15:13:45 +0800

    添加mybatis工程

---
+++ spring-boot-druid-mybatis/src/main/java/cn/iflyapi/SpringBootDruidMybatisApplication.java
@@ -0,0 +1,12 @@
+package cn.iflyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDruidMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDruidMybatisApplication.class, args);
+	}
+}
