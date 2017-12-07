cba5c2a9d332538927f3707801f99040e6163838
flyhero <qfwang666@163.com>
2017-12-07 15:05:07 +0800

    add jpa

---
+++ spring-boot-druid-jpa/src/main/java/cn/iflyapi/SpringBootDruidJpaApplication.java
@@ -0,0 +1,12 @@
+package cn.iflyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDruidJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDruidJpaApplication.class, args);
+	}
+}
