daf7fd5cd32294b11903290f6b04e2416d254a9e
flyhero <qfwang666@163.com>
2017-12-07 16:57:29 +0800

    添加定时任务工程

---
+++ spring-boot-task-quartz/src/main/java/cn/iflyapi/SpringBootTaskQuartzApplication.java
@@ -0,0 +1,12 @@
+package cn.iflyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskQuartzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskQuartzApplication.class, args);
+	}
+}
